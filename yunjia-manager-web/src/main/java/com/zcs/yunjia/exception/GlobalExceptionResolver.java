8b89f59977c6a9462f30a448e19081271722fa32
xuhaiyan <1614187761@qq.com>
2019-05-30 19:31:01 +0800

    解决后台管理系统bug

--- yunjia-manager-web/src/main/java/com/zcs/yunjia/exception/GlobalExceptionResolver.java
+++ yunjia-manager-web/src/main/java/com/zcs/yunjia/exception/GlobalExceptionResolver.java
@@ -12,10 +12,10 @@ public class GlobalExceptionResolver implements HandlerExceptionResolver {
     public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
         ModelAndView mav = new ModelAndView();
         mav.addObject("message","系统出错了...请稍后再试。");
-        System.out.println("-----------------------------------------------------------------");
+        System.out.println("--------------------------------------------------------------------------");
         System.out.print("manager-web-exception:");
         e.printStackTrace();
-        System.out.println("-----------------------------------------------------------------");
+        System.out.println("---------------------------------------------------------------------------");
         mav.setViewName("/error/exception");
         return mav;
     }
