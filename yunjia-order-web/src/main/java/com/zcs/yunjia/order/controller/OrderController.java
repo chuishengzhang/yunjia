f12e211c50d7f029b12e0c9dc035b5e21aa70939
xuhaiyan <1614187761@qq.com>
2019-08-15 10:21:24 +0800

    显示订单确认页面、db.properties

--- yunjia-order-web/src/main/java/com/zcs/yunjia/order/controller/OrderController.java
+++ yunjia-order-web/src/main/java/com/zcs/yunjia/order/controller/OrderController.java
@@ -1,13 +1,35 @@
 package com.zcs.yunjia.order.controller;
 
+import com.zcs.yunjia.cart.pojo.CartItem;
+import com.zcs.yunjia.cart.service.CartService;
+import com.zcs.yunjia.pojo.TbUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class OrderController {
 
+    @Autowired
+    private CartService cartService;
+
     @RequestMapping(value = {"/","/order"})
-    public String showPage(){
+    public String showPage(HttpServletRequest request, Model model){
+        //从request中获取拦截器写入的用户数据
+        TbUser user = (TbUser)request.getAttribute("user");
+        //从redis取购物车
+        List<CartItem> userCart = cartService.getUserCart(user.getId());
+        //图片处理
+        for (CartItem ci: userCart) {
+            if(ci.getImage().contains(",")){
+                ci.setImage(ci.getImage().split(",")[0]);
+            }
+        }
+        model.addAttribute("cartList",userCart);
         return "order-cart";
     }
 }
